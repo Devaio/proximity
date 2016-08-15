f786636636462fc9a3ad59ddc23a33913bada94f
Admin <admin@augur.io>
2016-08-15 13:16:34 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,66 @@
+'use strict';
+require('colors');
+  /////////////////////////////////////////////////////////////////////////////////////////////////////
+ /* this module is only good for development and not recommended for use in production         ///////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+ppppp   ppppppppp   rrrrr   rrrrrrrrr      ooooooooooo xxxxxxx      xxxxxxxyyyyyyy           yyyyyyy
+p::::ppp:::::::::p  r::::rrr:::::::::r   oo:::::::::::oox:::::x    x:::::x  y:::::y         y:::::y
+p:::::::::::::::::p r:::::::::::::::::r o:::::::::::::::ox:::::x  x:::::x    y:::::y       y:::::y
+pp::::::ppppp::::::prr::::::rrrrr::::::ro:::::ooooo:::::o x:::::xx:::::x      y:::::y     y:::::y
+ p:::::p     p:::::p r:::::r     r:::::ro::::o     o::::o  x::::::::::x        y:::::y   y:::::y
+ p:::::p     p:::::p r:::::r     rrrrrrro::::o     o::::o   x::::::::x          y:::::y y:::::y
+ p:::::p     p:::::p r:::::r            o::::o     o::::o   x::::::::x           y:::::y:::::y
+ p:::::p    p::::::p r:::::r            o::::o     o::::o  x::::::::::x           y:::::::::y
+ p:::::ppppp:::::::p r:::::r            o:::::ooooo:::::o x:::::xx:::::x           y:::::::y
+ p::::::::::::::::p  r:::::r            o:::::::::::::::ox:::::x  x:::::x           y:::::y
+ p::::::::::::::pp   r:::::r             oo:::::::::::oox:::::x    x:::::x         y:::::y
+ p::::::pppppppp     rrrrrrr               ooooooooooo xxxxxxx      xxxxxxx       y:::::y
+ p:::::p                                                                         y:::::y
+ p:::::p                                                                        y:::::y
+p:::::::p                                                                      y:::::y
+p:::::::p                                                                     y:::::y
+ppppppppp                                                                    yyyyyyy
+*//////////////////////////////////////////////////////////////////////////////////
+const proxyMap = {
+      //hostname: port,
+      //hostname: port
+    };
+
+function route( req, res, bounce ) {
+    let host = req.headers.host;
+    let port = proxyMap[ host ];
+
+    if ( host !== undefined && port !== undefined ) {
+        bounce({
+            port: port,
+            headers: {
+                // this is a hack and is the reason why this
+                // module should only be use in development
+                'Connection': 'close',
+                'X-Forwarded-Proto': 'https'
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.end( 'Could not resolve host: ' + host + ', port: ' + port );
+    }
+    console.log( '# Routing:'.green, host, ':', port, req.url );
+}
+function reportError ( error ) {
+    if ( error ) {
+        console.error('#ERROR'.bold.red, error );
+    }
+}
+
+require('bouncy')( route ).listen( 80 ).on('error', reportError );
+require('bouncy')({
+    ca: // your certficate authority or issuer certificate,
+    cert: //your wildcard or ssl cert
+    key:  // Private key
+}, route ).listen( 443 ).on( 'error', reportError );
+
+console.log('\n'+
+'==========================\n'+
+' # Proxy Server Started # \n'.cyan+
+'==========================\n'
+);
